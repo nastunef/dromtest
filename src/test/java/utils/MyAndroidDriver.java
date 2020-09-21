30b37b443abea2a26713ad160ca96334cfca4fc6
nastunef <nastunef@mail.ru>
2020-09-21 07:09:12 +0700

    init

--- src/test/java/utils/MyAndroidDriver.java
+++ src/test/java/utils/MyAndroidDriver.java
@@ -1,8 +1,6 @@
 package utils;
 
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
